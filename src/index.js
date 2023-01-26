c6e1aea2301aa9dbb244031692df13de617fffa7
igorlebedev <lebedew.igor400@yandex.ru>
2023-01-26 21:45:29 +0700

    change main logic

--- src/index.js
+++ src/index.js
@@ -2,31 +2,27 @@ import fs from 'fs';
 import _ from 'lodash';
 import path from 'path';
 
-const getPath = (filename) => {
-  return path.resolve(process.cwd(), filename);
-};
+const getPath = (filename) => path.resolve(process.cwd(), filename);
 
-export const genDiff = (file1, file2) => {
-  const file1coll = JSON.parse(fs.readFileSync(getPath(file1)));
-  const file2coll = JSON.parse(fs.readFileSync(getPath(file2)));
-  const keys1 = Object.keys(file1coll);
-  const keys2 = Object.keys(file2coll);
+const genDiff = (file1, file2) => {
+  const file1Coll = JSON.parse(fs.readFileSync(getPath(file1)));
+  const file2Coll = JSON.parse(fs.readFileSync(getPath(file2)));
+  const keys1 = Object.keys(file1Coll);
+  const keys2 = Object.keys(file2Coll);
   const allSortedKeys = _.sortBy(_.union(keys1, keys2));
 
-  console.log(allSortedKeys)
-
   const changes = allSortedKeys.reduce((acc, key) => {
-    if (!_.has(file1coll, key)) {
-      const plus = `+ ${key}`
-      acc[plus] = file2coll[key]
-    } else if (!_.has(file2coll, key)) {
-      acc[`- ${key}`] = file1coll[key]
-    } else if (file1coll[key] !== file2coll[key]) {
-      acc[`- ${key}`] = file1coll[key];
-      acc[`+ ${key}`] = file2coll[key];
-    } else acc[key] = file1coll[key];
+    if (!_.has(file1Coll, key)) {
+      acc += `\n- ${key}: ${file2Coll[key]}`;
+    } else if (!_.has(file2Coll, key)) {
+      acc += `\n- ${key}: ${file1Coll[key]}`;
+    } else if (file1Coll[key] !== file2Coll[key]) {
+      acc += `\n- ${key}: ${file1Coll[key]}`;
+      acc += `\n+ ${key}: ${file2Coll[key]}`;
+    } else acc += `\n  ${key}: ${file1Coll[key]}`;
     return acc;
-  }, {});
-  return changes;
+  }, '');
+  return `{${changes}\n}`;
 };
 
+export default genDiff;
