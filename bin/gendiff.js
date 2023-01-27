0eea92f4b7eea4020c096a000e5e755b25f6c5f9
igorlebedev <lebedew.igor400@yandex.ru>
2023-01-27 12:29:30 +0700

    minor fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,7 @@ import genDiff from '../src/index.js';
 
 program
   .name('gendiff')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .arguments('<filepath1> <argument2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1', '-v, --version', 'output the current version')
   .option('-f, --format <type>', 'output format', 'json')
