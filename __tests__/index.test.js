268c9924d26dbb4736c67aeae618dbbb2e02f9b1
igorlebedev <lebedew.igor400@yandex.ru>
2023-01-30 16:17:35 +0700

    add test

---
+++ __tests__/index.test.js
@@ -0,0 +1,14 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+import expected from '../__fixtures__/expect.js';
+import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('gendiff-test', () => {
+  expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(expected);
+});
