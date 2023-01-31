6b4b4886a7b8895a665917aea690cb4cad6aefc1
igorlebedev <lebedew.igor400@yandex.ru>
2023-01-31 13:34:35 +0700

    add yaml test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,6 +8,10 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('gendiff-test', () => {
+test('gendiff-test-json', () => {
   expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(expected);
+});
+
+test('gendiff-test-yaml', () => {
+  expect(gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'))).toBe(expected);
 });
