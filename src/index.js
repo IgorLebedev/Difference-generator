4a856cd27b975d1a62049aaeb04256efb2a3da24
igorlebedev <lebedew.igor400@yandex.ru>
2023-02-02 18:25:06 +0700

    refactor

--- src/index.js
+++ src/index.js
@@ -1,62 +1,20 @@
-// import fs from 'fs';
-import _ from 'lodash';
-// import path from 'path';
+import fs from 'fs';
+import path from 'path';
 import parse from './parser.js';
-// import formatter from './formatter.js';
-import stylish from './formatter.js';
+import astBuilder from './astbuilder.js';
+import formatter from './fomatters/index.js';
+
+const getAbsolutePath = (pathToFile) => path.resolve(process.cwd(), pathToFile);
+
+const readFile = (file) => fs.readFileSync(file);
+
+const getExtension = (file) => path.extname(file);
 
 const genDiff = (file1, file2, format = 'stylish') => {
-  const file1Coll = parse(file1);
-  const file2Coll = parse(file2);
-  const iter = (first, second) => {
-    const keys1 = Object.keys(first);
-    const keys2 = Object.keys(second);
-    const allSortedKeys = _.sortBy(_.union(keys1, keys2));
-    const changes = allSortedKeys.reduce((acc, key) => {
-      if (!_.has(first, key)) {
-        acc.push({
-          name: key,
-          type: 'added',
-          value: _.cloneDeep(second[key]),
-        });
-      } if (!_.has(second, key)) {
-        acc.push({
-          name: key,
-          type: 'deleted',
-          value: _.cloneDeep(first[key]),
-        });
-      }
-      if (first[key] !== second[key] && Object.hasOwn(first, key) && Object.hasOwn(second, key) && (!_.isObject(first[key]) || !_.isObject(second[key]))) {
-        acc.push({
-          name: key,
-          type: 'changed',
-          from: _.cloneDeep(first[key]),
-          to: _.cloneDeep(second[key]),
-        });
-      }
-      if (first[key] === second[key]) {
-        acc.push({
-          name: key,
-          type: 'unchanged',
-          value: _.cloneDeep(first[key]),
-        });
-      }
-      if (_.isObject(first[key]) && _.isObject(second[key])) {
-        acc.push({
-          name: key,
-          type: 'nested',
-          children: iter(first[key], second[key]),
-        });
-      }
-      return acc;
-    }, []);
-    return changes;
-  };
-  switch (format) {
-    case 'stylish':
-      return stylish(iter(file1Coll, file2Coll));
-    default: throw new Error('unexpected format');
-  }
+  const file1Coll = parse(readFile(getAbsolutePath(file1)), getExtension(file1));
+  const file2Coll = parse(readFile(getAbsolutePath(file2)), getExtension(file2));
+  const astTree = astBuilder(file1Coll, file2Coll);
+  return formatter(astTree, format);
 };
 
 export default genDiff;
