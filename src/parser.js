7ceb69a622ad13b3276e3f7b7ecbbbfe1e85700d
igorlebedev <lebedew.igor400@yandex.ru>
2023-02-02 18:25:14 +0700

    change parser logic

--- src/parser.js
+++ src/parser.js
@@ -1,18 +1,13 @@
-import fs from 'fs';
-import path from 'path';
 import yaml from 'js-yaml';
 
-const getAbsolutePath = (pathToFile) => path.resolve(process.cwd(), pathToFile);
-
-const readFile = (file) => fs.readFileSync(file);
-
-const getExtension = (file) => path.extname(file);
-
-export default (file) => {
-  const format = getExtension(file);
-  if (format === '.json') {
-    return JSON.parse(readFile(getAbsolutePath(file)));
-  } if (format === '.yml' || format === '.yaml') {
-    return yaml.load(readFile(getAbsolutePath(file)));
-  } throw new Error('This format doesn\'t support');
+export default (data, format) => {
+  switch (format) {
+    case '.json':
+      return JSON.parse(data);
+    case '.yml':
+      return yaml.load(data);
+    case 'yaml':
+      return yaml.load(data);
+    default: throw new Error(`This format doesn't support: ${format}`);
+  }
 };
