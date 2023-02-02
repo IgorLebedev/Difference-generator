e18a93629c15b3c1e0d1f262643f0805199916fd
igorlebedev <lebedew.igor400@yandex.ru>
2023-02-02 20:21:55 +0700

    add some tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,16 +8,30 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('gendiff-test-json', () => {
+test('gendiff-test-json-stylish', () => {
   const recieveJson = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const expected = fs.readFileSync(getFixturePath('expect.txt'), 'utf-8');
+  const expected = fs.readFileSync(getFixturePath('expect-stylish.txt'), 'utf-8');
 
   expect(recieveJson).toBe(expected);
 });
 
-const recieveYaml = gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'));
-const expected = fs.readFileSync(getFixturePath('expect.txt'), 'utf-8');
+test('gendiff-test-yaml-stylish', () => {
+  const recieveYaml = gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'));
+  const expected = fs.readFileSync(getFixturePath('expect-stylish.txt'), 'utf-8');
 
-test('gendiff-test-yaml', () => {
+  expect(recieveYaml).toBe(expected);
+});
+
+test('gendiff-test-json-plain', () => {
+  const recieveJson = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
+  const expected = fs.readFileSync(getFixturePath('expect-plain.txt'), 'utf-8');
+
+  expect(recieveJson).toBe(expected);
+});
+
+test('gendiff-test-yaml-plain', () => {
+  const recieveYaml = gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'), 'plain');
+  const expected = fs.readFileSync(getFixturePath('expect-plain.txt'), 'utf-8');
+
   expect(recieveYaml).toBe(expected);
 });
