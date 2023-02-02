725a54a6dced66fd2da1a6e61033fdd039c7cf2a
igorlebedev <lebedew.igor400@yandex.ru>
2023-02-02 16:13:47 +0700

    parser rework

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,18 @@
+import fs from 'fs';
+import path from 'path';
 import yaml from 'js-yaml';
 
-export default (file, format) => {
+const getAbsolutePath = (pathToFile) => path.resolve(process.cwd(), pathToFile);
+
+const readFile = (file) => fs.readFileSync(file);
+
+const getExtension = (file) => path.extname(file);
+
+export default (file) => {
+  const format = getExtension(file);
   if (format === '.json') {
-    return JSON.parse(file);
+    return JSON.parse(readFile(getAbsolutePath(file)));
   } if (format === '.yml' || format === '.yaml') {
-    return yaml.load(file);
+    return yaml.load(readFile(getAbsolutePath(file)));
   } throw new Error('This format doesn\'t support');
 };
