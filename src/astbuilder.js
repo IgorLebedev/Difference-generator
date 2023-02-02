008fb6c09c94537ee77638f3156f752b2c8c5288
igorlebedev <lebedew.igor400@yandex.ru>
2023-02-02 23:29:11 +0700

    minor fix

--- src/astbuilder.js
+++ src/astbuilder.js
@@ -4,19 +4,6 @@ const astBuilder = (first, second) => {
   const keys = [Object.keys(first), Object.keys(second)];
   const allSortedKeys = _.sortBy(_.union(...keys));
   return allSortedKeys.map((key) => {
-    if (!_.has(first, key)) {
-      return {
-        name: key,
-        type: 'added',
-        value: (second[key]),
-      };
-    } if (!_.has(second, key)) {
-      return {
-        name: key,
-        type: 'deleted',
-        value: (first[key]),
-      };
-    }
     if (_.isObject(first[key]) && _.isObject(second[key])) {
       return {
         name: key,
@@ -24,17 +11,30 @@ const astBuilder = (first, second) => {
         children: astBuilder(first[key], second[key]),
       };
     }
+    if (!_.has(first, key)) {
+      return {
+        name: key,
+        type: 'added',
+        value: second[key],
+      };
+    } if (!_.has(second, key)) {
+      return {
+        name: key,
+        type: 'deleted',
+        value: first[key],
+      };
+    }
     if (!_.isEqual(first[key], second[key])) {
       return {
         name: key,
         type: 'updated',
-        from: (first[key]),
-        to: (second[key]),
+        from: first[key],
+        to: second[key],
       };
     } return {
       name: key,
       type: 'unchanged',
-      value: (first[key]),
+      value: first[key],
     };
   });
 };
