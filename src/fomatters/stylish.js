13ebb4c25905d2f9c1b7f07f4f64f99437a51452
igorlebedev <lebedew.igor400@yandex.ru>
2023-02-09 15:37:51 +0700

    refactor stylish

--- src/fomatters/stylish.js
+++ src/fomatters/stylish.js
@@ -1,31 +1,33 @@
 import _ from 'lodash';
 
+const spacesWithSign = (multiplier) => ' '.repeat(4 * multiplier - 2);
+
+const spacesWithoutSign = (multiplier) => ' '.repeat(4 * multiplier);
+
+const checkObj = (node, multiplier) => {
+  if (_.isObject(node)) {
+    const entries = Object.entries(node);
+    const deepObjects = entries
+      .reduce((acc, [key, value]) => `${acc}\n${spacesWithoutSign(multiplier)}${key}: ${checkObj(value, multiplier + 1)}`, '');
+    return `{${deepObjects}\n${spacesWithoutSign(multiplier - 1)}}`;
+  } return node;
+};
+
 const stylish = (ast) => {
   const iter = (tree, multiplier) => {
-    const spacesWithoutSign = ' '.repeat(4 * multiplier);
-    const spacesWithSign = ' '.repeat(4 * multiplier - 2);
-    const closingBracketSpace = ' '.repeat(4 * (multiplier - 1));
-    if (!_.isArray(tree)) {
-      if (_.isObject(tree)) {
-        const entries = Object.entries(tree);
-        const deepObjects = entries
-          .reduce((acc, [key, value]) => `${acc}\n${spacesWithoutSign}${key}: ${iter(value, multiplier + 1)}`, '');
-        return `{${deepObjects}\n${closingBracketSpace}}`;
-      } return tree;
-    }
     const result = tree.map((node) => {
       switch (node.type) {
         case 'nested':
-          return `${spacesWithoutSign}${node.name}: ${(iter(node.children, multiplier + 1))}`;
+          return `${spacesWithoutSign(multiplier)}${node.name}: ${(iter(node.children, multiplier + 1))}`;
         case 'unchanged':
-          return `${spacesWithoutSign}${node.name}: ${iter(node.value, multiplier + 1)}`;
+          return `${spacesWithoutSign(multiplier)}${node.name}: ${checkObj(node.value, multiplier + 1)}`;
         case 'added':
-          return `${spacesWithSign}+ ${node.name}: ${iter(node.value, multiplier + 1)}`;
+          return `${spacesWithSign(multiplier)}+ ${node.name}: ${checkObj(node.value, multiplier + 1)}`;
         case 'deleted':
-          return `${spacesWithSign}- ${node.name}: ${iter(node.value, multiplier + 1)}`;
+          return `${spacesWithSign(multiplier)}- ${node.name}: ${checkObj(node.value, multiplier + 1)}`;
         case 'updated': {
-          const from = `${spacesWithSign}- ${node.name}: ${iter(node.from, multiplier + 1)}`;
-          const to = `${spacesWithSign}+ ${node.name}: ${iter(node.to, multiplier + 1)}`;
+          const from = `${spacesWithSign(multiplier)}- ${node.name}: ${checkObj(node.from, multiplier + 1)}`;
+          const to = `${spacesWithSign(multiplier)}+ ${node.name}: ${checkObj(node.to, multiplier + 1)}`;
           return `${from}\n${to}`;
         }
         default: throw new Error(`unexpected node type: ${node.type}`);
@@ -34,7 +36,7 @@ const stylish = (ast) => {
     return [
       '{',
       ...result,
-      `${closingBracketSpace}}`,
+      `${spacesWithoutSign(multiplier - 1)}}`,
     ].join('\n');
   };
   return iter(ast, 1);
