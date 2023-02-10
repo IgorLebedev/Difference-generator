139def85da105a30f01c78fd33088d5e8de17727
igorlebedev <lebedew.igor400@yandex.ru>
2023-02-10 20:59:26 +0700

    rework ast

--- src/astbuilder.js
+++ src/astbuilder.js
@@ -5,22 +5,26 @@ const astBuilder = (first, second) => {
   return allSortedKeys.map((key) => {
     const result = {
       name: key,
+      type: null,
+      children: null,
+      value: [],
     };
     if (!_.has(second, key)) {
       result.type = 'deleted';
-      result.value = first[key];
+      result.value.push(first[key]);
     } else if (!_.has(first, key)) {
       result.type = 'added';
-      result.value = second[key];
+      result.value.push(second[key]);
     } else if (_.isObject(first[key]) && _.isObject(second[key])) {
       result.type = 'nested';
       result.children = astBuilder(first[key], second[key]);
     } else if (!_.isEqual(first[key], second[key])) {
       result.type = 'updated';
-      result.value = [first[key], second[key]];
+      result.value.push(first[key]);
+      result.value.push(second[key]);
     } else if (first[key] === second[key]) {
       result.type = 'unchanged';
-      result.value = first[key];
+      result.value.push(first[key]);
     }
     return result;
   });
