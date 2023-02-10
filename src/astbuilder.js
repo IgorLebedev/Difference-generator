ea89f2f334f308337059bfb1917afa9c657a0d73
igorlebedev <lebedew.igor400@yandex.ru>
2023-02-10 16:10:35 +0700

    refactor

--- src/astbuilder.js
+++ src/astbuilder.js
@@ -1,24 +1,8 @@
 import _ from 'lodash';
 
 const astBuilder = (first, second) => {
-  const keys = [Object.keys(first), Object.keys(second)];
-  const allSortedKeys = _.sortBy(_.union(...keys));
+  const allSortedKeys = _.sortBy(_.union(Object.keys(first), Object.keys(second)));
   return allSortedKeys.map((key) => {
-    if (_.isObject(first[key]) && _.isObject(second[key])) {
-      return {
-        name: key,
-        type: 'nested',
-        children: astBuilder(first[key], second[key]),
-      };
-    }
-    if (!_.isEqual(first[key], second[key]) && _.has(first, key) && _.has(second, key)) {
-      return {
-        name: key,
-        type: 'updated',
-        from: first[key],
-        to: second[key],
-      };
-    }
     if (!_.has(first, key)) {
       return {
         name: key,
@@ -26,9 +10,31 @@ const astBuilder = (first, second) => {
         value: second[key],
       };
     }
+    if (!_.has(second, key)) {
+      return {
+        name: key,
+        type: 'deleted',
+        value: first[key],
+      };
+    }
+    if (_.isObject(first[key]) && _.isObject(second[key])) {
+      return {
+        name: key,
+        type: 'nested',
+        children: astBuilder(first[key], second[key]),
+      };
+    }
+    if (!_.isEqual(first[key], second[key])) {
+      return {
+        name: key,
+        type: 'updated',
+        value1: first[key],
+        value2: second[key],
+      };
+    }
     return {
       name: key,
-      type: !_.has(second, key) ? 'deleted' : 'unchanged',
+      type: 'unchanged',
       value: first[key],
     };
   });
